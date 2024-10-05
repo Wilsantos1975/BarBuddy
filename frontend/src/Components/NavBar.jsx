0ecbd1114a977970945f6e688b77a4b3bdeade7d
Your Name <Faustosantos@pursuit.org>
2024-10-05 16:15:09 -0400

    fixed tailwind issue. Auto prefixer needed to be installed

---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="bg-red-500 p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-white text-xl font-bold">MyApp</Link>
+        <ul className="flex space-x-4">
+          <li><Link to="/calculator" className="text-white hover:text-gray-300">Calculator</Link></li>
+          <li><Link to="/about" className="text-white hover:text-gray-300">About the App</Link></li>
+          <li><Link to="/tutorials" className="text-white hover:text-gray-300">Tutorials</Link></li>
+          <li><Link to="/event-wizard" className="text-white hover:text-gray-300">Event Wizard</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
