16a86a029a1d7e554e0ffa34b5ebdc0d2d314ecb
Wilsantos1975 <Faustosantos@pursuit.org>
2024-10-15 19:41:33 -0400

    prosgres issue corrected. database was not connected properly. set up a local connection as oppsed to an external.

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,23 +1,29 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const dbURL = process.env.DB_URL;
+// const dbURL = process.env.DB_URL;
 
-if (!dbURL) {
-    console.error("DB_URL is not defined in the environment variables.");
-    process.exit(1); // Exit the process if DB_URL is not set
-}
-
-const cn = {
-    connectionString: dbURL,
-    allowExitOnIdle: true,
-    max: 30,
-    ssl: {
-        rejectUnauthorized: false // Adjust based on your SSL requirements
-    }
+// if (!dbURL) {
+//     console.error("DB_URL is not defined in the environment variables.");
+//     process.exit(1); // Exit the process if DB_URL is not set
+// }
+const databaseConfig = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER
 };
 
-const db = pgp(cn);
+// const cn = {
+//     connectionString: dbURL,
+//     allowExitOnIdle: true,
+//     max: 30,
+//     ssl: {
+//         rejectUnauthorized: false // Adjust based on your SSL requirements
+//     }
+// };
+
+const db = pgp(databaseConfig);
 
 // Test the connection
 db.connect()
